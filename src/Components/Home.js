8ab4e32966c4a49ccde58126ea1004233629f373
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2024-02-04 18:45:10 +0200

    landing-page

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <h3>Home</h3>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../Assets/Logo.svg';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2>Navbar</h2>
+        </div>
+    );
+};
+
+export default Navbar;
